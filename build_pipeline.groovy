b69e86b4c5047bd81a78a6679035de0330982b39
Anil <anilkumawat@gmail.com>
2025-03-25 15:38:22 +0530

    commit

---
+++ build_pipeline.groovy
@@ -0,0 +1,30 @@
+node {
+    try {
+        // Stage 1: Build Docker image
+        stage('Build') {
+            echo "Building Docker image"
+            sh 'docker build -t $DOCKER_USERNAME/web_application:$DOCKER_TAG .'
+        }
+
+        // Stage 2: Login to Docker Hub
+        stage('Login to Docker Hub') {
+            echo "Logging into Docker Hub"
+            // Secure login using password from environment variables
+            sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
+        }
+
+        // Stage 3: Push the image to Docker Hub
+        stage('Push to Docker Hub') {
+            echo "Pushing Docker image to Docker Hub"
+            // Push the image to Docker Hub
+            sh 'docker push $DOCKER_USERNAME/web_application:$DOCKER_TAG'
+        }
+
+    } catch (Exception e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    } finally {
+        echo 'Pipeline completed.'
+    }
+}
+
