68dd9d991531326e041543e297b4b1043fab18d3
Anil <anilkumawat@gmail.com>
2025-03-25 16:00:45 +0530

    commit

---
+++ deploy_pipeline.groovy
@@ -0,0 +1,32 @@
+node {
+    try {
+        // Stage 1: Login to Docker Hub
+        stage('Login to Docker Hub') {
+            echo "Login to Docker Hub"
+            sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
+        }
+
+        // Stage 2: Pull Image
+        stage('Pull Image') {
+            echo "Pulling Docker image..."
+            sh 'docker pull $DOCKER_USERNAME/web_application:$DOCKER_TAG'
+        }
+
+        // Stage 3: Deploy
+        stage('Deploy') {
+            echo "Deploying the application..."
+            sh 'docker rm -f web-app || true'  
+            sh 'docker run -d -p 5000:5000 --name web-app $DOCKER_USERNAME/web_application:$DOCKER_TAG'  
+            sh 'docker ps'  
+            sh 'pwd'  
+        }
+
+    } catch (Exception e) {
+        currentBuild.result = 'FAILURE'
+        throw e 
+    } finally {
+        echo 'Pipeline completed' 
+    }
+}
+
+	
