51ca5cea290646a39cb95f5f66df3dd2322bf2da
Anil <anilkumawat@gmail.com>
2025-03-26 18:48:33 +0530

    commit

--- deploy_pipeline.groovy
+++ deploy_pipeline.groovy
@@ -8,8 +8,7 @@ properties([
 node {
     try {
 	def image
-	def command
-	def container
+	def contain
         // Stage 1: Login to Docker Hub
         stage('Login to Docker Hub') {
             withDockerRegistry([credentialsId: 'docker_hub_credentials']) {
@@ -31,9 +30,9 @@ node {
         // Stage 3: Deploy
         stage('Deploy') {
             echo "Deploying the application"
-            container = docker.container("web-app")
-	    container.stop()
-	    container.remove()
+            contain = docker.container("web-app")
+	    contain.stop()
+	    contain.remove()
 	   // process.waitFor()
             sh 'docker run -d -p 5000:5000 --name web-app $DOCKER_USERNAME/web_application:$DOCKER_TAG'  
             sh 'docker ps'  
