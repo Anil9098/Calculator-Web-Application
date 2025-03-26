2e522b74f8204d674ac3453c10600018207b69c1
Anil <anilkumawat@gmail.com>
2025-03-26 17:02:30 +0530

    commit

--- build_pipeline.groovy
+++ build_pipeline.groovy
@@ -8,24 +8,14 @@ properties([
 node {
     try {
 
-	def image
-	//def dockertag = params.DOCKER_TAG ?: 'latest'
-	//echo "docker image tag: $DOCKER_TAG"
-
-        // Stage 1: Build Docker image
-	
-       // stage('Code Clone') {
-         //   echo "cloning git repository"
-	 //   withCredentials([gitUsernamePassword(credentialsId: 'gitCredentialsId',url: "https://github.com/Anil9098/Calculator-Web-Application.git")])
-	    
-	stage('Checkout') {
-          // Checkout the private repository using Git with credentials
+	def image	    
+	stage('Code Clone') {
+          // git clone
           checkout([$class: 'GitSCM',
 	    branches: [[name: '*/master']],  
             userRemoteConfigs: [[
                 url: "https://github.com/Anil9098/Calculator-Web-Application.git",
-		credentialsId: "gitCredentialsId"
-		
+		credentialsId: "gitCredentialsId"		
             ]]
          ])
        }
@@ -35,32 +25,31 @@ node {
             image = docker.build("${DOCKER_USERNAME}/web_application:${DOCKER_TAG}")
         }
 	
-        // Stage 2: Login to Docker Hub
+        // login to docker hub
         stage('Login to Docker Hub') {
             withDockerRegistry([credentialsId: 'docker_hub_credentials']) {
                 echo "Successfully logged into Docker Hub"
+		echo "pushing docker image"
+		image.push()
             }
         }
 
-       // Stage 3: Push the image to Docker Hub
-       // stage('Push to Docker Hub') {
+       // stage('push to docker hub') {
 
-	   // docker.withRegistry("[credentialsId: 'docker_hub_credentials']") {
-           //    image.push()
-         //  echo "Pushing Docker image to Docker Hub"
-          // ("${DOCKER_USERNAME}/web_application:${DOCKER_TAG}").push()
-         //   } 
-       // }
+	  // docker.withRegistry("[credentialsId: 'docker_hub_credentials']") {
+         //      image.push()
+	//}
 
 
-	stage('Push to Docker Hub') {
-            withCredentials([usernamePassword(credentialsId: 'docker_hub_credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                docker.withRegistry('https://index.docker.io/v1/', 'docker_hub_credentials') {
-                    echo "Pushing Docker image to Docker Hub"
-                    image.push()
-                }
-            }
-        }
+//	stage('Push to Docker Hub') {
+//            withCredentials([usernamePassword(credentialsId: 'docker_hub_credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+//                docker.withRegistry('https://index.docker.io/v1/', 'docker_hub_credentials') {
+//                    echo "Pushing Docker image to Docker Hub"
+//                    image.push()
+//                }
+//            }
+//        }
+
 
     } catch (Exception e) {
         currentBuild.result = 'FAILURE'
