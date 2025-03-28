a4612efb3fc92360568a7e02e7d907a6f1f4fbca
Anil <anilkumawat@gmail.com>
2025-03-28 19:04:30 +0530

    commit

--- build_pipeline.groovy
+++ build_pipeline.groovy
@@ -8,7 +8,7 @@ properties([
 node {
     try {
     
-	sh "ssh -i $key ubuntu@13.233.100.250"
+	//sh "ssh -i $key ubuntu@13.233.100.250"
 	def image	    
 	stage('Code Clone') {
           // git clone
