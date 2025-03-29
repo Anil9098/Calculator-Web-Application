e845062b8ecddfd4d7c6f60be4725fdf067045e9
Anil <anilkumawat@gmail.com>
2025-03-29 18:37:09 +0530

    commit

--- build.groovy
+++ build.groovy
@@ -1,7 +1,13 @@
 node {
     try {
         stage('Run Script') {
-            sh "sudo ./deploy_ssh.sh"
+
+            sh '''
+                sudo ssh -i /root/.ssh/id_rsa ubuntu@13.233.100.250 
+                # Commands to run on the EC2 instance
+                echo "Running script on EC2 instance"
+                git clone https://github.com/Anil9098/Calculator-Web-Application.git
+            '''
         }
 
     } catch (Exception e) {
