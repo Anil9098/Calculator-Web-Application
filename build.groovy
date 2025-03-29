70b44871120228b8d38d668f1b3b173fbf5eadf3
Anil <anilkumawat@gmail.com>
2025-03-29 19:12:18 +0530

    commit

--- build.groovy
+++ build.groovy
@@ -1,13 +1,31 @@
 node {
     try {
-        stage('Run Script on EC2') {
+        stage('Run Script') {
 
-            // Ensure all commands run inside the SSH session on the EC2 instance
-            sh '
-                ./deploy_ssh.sh
-            '
+            sh '''
+                sudo ssh -i /root/.ssh/id_rsa ubuntu@13.233.100.250 <<'EOF'
+                # Commands to run inside EC2
+                
+                echo "Running script on EC2 instance"
+                rm -rf Calculator-Web-Application
+                
+                git clone https://github.com/Anil9098/Calculator-Web-Application.git
+                cd Calculator-Web-Application || { echo "Failed to enter directory"; exit 1; }
+                                
+                # Additional commands you want to run on EC2
+                docker kill app-container
+                docker system prune -a -f
+                
+                echo "Building Docker image..."
+                docker build -t web_application:latest . || { echo "Docker build failed"; exit 1; }
+                                
+                # Running Docker container
+                docker run -d -p 5000:5000 --name app-container web_application:latest || { echo "Docker run failed"; exit 1; }
+
+                EOF
+            '''
         }
-        
+
     } catch (Exception e) {
         currentBuild.result = 'FAILURE'
         throw e
@@ -15,3 +33,6 @@ node {
         echo 'Pipeline completed'
     }
 }
+
+
+
