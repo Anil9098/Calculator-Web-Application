4a50f9d387f03a5292d81bbc489fa50293c5085c
Anil Kumawat <90092212+Anil9098@users.noreply.github.com>
2025-03-29 18:47:10 +0530

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -7,10 +7,13 @@ node {
                 sudo ssh -i /root/.ssh/id_rsa ubuntu@13.233.100.250 <<EOF
                 # Commands to run inside EC2
                 echo "Running script on EC2 instance"
+                rm -rf Calculator-Web-Application
                 git clone https://github.com/Anil9098/Calculator-Web-Application.git
                 cd Calculator-Web-Application || { echo "Failed to enter directory"; exit 1; }
                 
                 # Additional commands you want to run on EC2
+                docker kill app-container
+                docker system prune -a -f
                 echo "Building Docker image..."
                 docker build -t web_application:latest . || { echo "Docker build failed"; exit 1; }
                 
