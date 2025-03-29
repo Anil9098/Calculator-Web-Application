0f76118a315a8720bb1b80127c2769ad14baa2c7
Anil Kumawat <90092212+Anil9098@users.noreply.github.com>
2025-03-29 18:39:32 +0530

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -6,6 +6,7 @@ node {
                 sudo ssh -i /root/.ssh/id_rsa ubuntu@13.233.100.250 
                 # Commands to run on the EC2 instance
                 echo "Running script on EC2 instance"
+                pwd
                 git clone https://github.com/Anil9098/Calculator-Web-Application.git
             '''
         }
