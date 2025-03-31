e5dae5544baeba7e8c78d545381b7492501a5118
Anil <anilkumawat@gmail.com>
2025-03-31 15:04:06 +0530

    commit

--- build_ssh_pipeline.groovy
+++ build_ssh_pipeline.groovy
@@ -3,6 +3,7 @@ node {
         stage('SSH on EC2') {
             //sh "cd /home/ncs/Anil/web_app_deploy_script/bash"
             //sh "cd /home/ncs/Anil/web_app_deploy_script/bash"
+            pwd
             echo "path fixed"
             sh "./deploy_ssh.sh"
         }
