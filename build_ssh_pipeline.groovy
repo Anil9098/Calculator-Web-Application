8ecf88720a71035ebb0666bd9bb26f34b4c0f35b
Anil <anilkumawat@gmail.com>
2025-03-31 18:01:37 +0530

    commit

--- build_ssh_pipeline.groovy
+++ build_ssh_pipeline.groovy
@@ -5,7 +5,18 @@ node {
             //sh "cd /home/ncs/Anil/web_app_deploy_script/bash"
             //sh"pwd"
             //echo "path fixed"
-            sh "sudo ./deploy_ssh.sh"
+            sh "public_ip=$(aws ec2 describe-instances --query "Reservations[].Instances[].PublicIpAddress" --output text)"
+            sh '''for ip in $public_ip; do
+                    echo "Processing IP: $ip"
+                    ssh -i "/home/ncs/Downloads/jenkinsnodekey.pem" ubuntu@$ip <<EOF
+                    rm -rf anil_practice
+                    git clone https://github.com/Anil9098/anil_practice.git
+                    cd anil_practice/bash
+                    ./example_deployment.sh
+                EOF
+                done
+            '''           
+            //sh "sudo ./deploy_ssh.sh"
         }
 
     } catch (Exception e) {
